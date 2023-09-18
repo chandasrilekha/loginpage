cdb1d7ec5544dd0db06238c6daa9133136728fb4
Srilekha Chanda <srilekha.chanda@gspann.com>
2023-09-19 02:04:48 +0530

    newly added changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component,OnInit} from '@angular/core';
 import { FormGroup,FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +9,7 @@ import { FormGroup,FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   public loginForm!:FormGroup
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder,private http:HttpClient,private router:Router){}
   
   ngOnInit():void{
   this.loginForm=this.formBuilder.group({
@@ -15,5 +17,23 @@ export class LoginComponent implements OnInit {
   password:['']
   })
   }
-  
+  login(){
+    this.http.get<any>("http://localhost:3000/signupusers")
+    .subscribe(res=>{
+    const user=res.find((a:any)=>{
+    return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+    });
+    if(user){
+      alert("Login Success");
+      this.loginForm.reset();
+      this.router.navigate(['dashbord'])
+      }
+      else{
+      alert("user not found");
+      }
+    },err=>{
+      alert("something went wrong")
+      }
+    )
+  }
 }
